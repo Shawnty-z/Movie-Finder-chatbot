d3c9c1e1e673eb507d62c8419f53b5901db877cc
Xuanye Zeng <zxy2053241527@gmail.com>
2025-06-24 10:38:17 +0100

    updated js

--- script.js
+++ script.js
@@ -168,16 +168,17 @@ async function fetchMovieRecommendation(genreId) {
 // Generate movie recommendation response
 function generateMovieResponse(movie) {
     const year = movie.releaseDate ? new Date(movie.releaseDate).getFullYear() : 'Unknown';
-    const rating = movie.rating ? `⭐ ${movie.rating}/10` : '';
+    const rating = movie.rating ? `⭐ ${movie.rating.toFixed(1)}/10` : '';
     
-    let response = `🎬 <strong>${movie.title}</strong> (${year}) ${rating}\n\n`;
-    
-    if (movie.overview) {
-        response += movie.overview;
-    } else {
-        response += "A great movie in this genre! Unfortunately, I don't have a description available.";
+    let response = `🎬 <strong>${movie.title}</strong> (${year})`;
+    if (rating) {
+        response += `<br>${rating}`;
     }
-    
+    if (movie.overview) {
+        response += `<br>${movie.overview}`;
+    } else {
+        response += `<br>A great movie in this genre! Unfortunately, I don't have a description available.`;
+    }
     return response;
 }
 
